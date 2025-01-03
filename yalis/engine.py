c7e016e092bf4c112b72c824fcf14d3c3fd6d54f
Akarsh Srivastava <akarsh@login18.chn.perlmutter.nersc.gov>
2025-01-03 13:49:16 -0800

    adding stopping at eos logic

--- yalis/engine.py
+++ yalis/engine.py
@@ -216,6 +216,10 @@ class LLMEngine:
                 "prompts must be either a list of strings or a list of lists of integers"
             )
 
+        # Initialize batch size and done mask for multiple batches
+        batch_size = prompt_tokens.size(0)
+        if batch_size > 1:
+            done_mask = torch.zeros(batch_size, dtype=torch.bool, device="cuda")
         output_tokens = []
         # Start timing the operations
         start = torch.cuda.Event(enable_timing=True)
@@ -253,6 +257,21 @@ class LLMEngine:
                         next_token
                     )  # Copy the new token into tokens
                 output_tokens.append(next_token.clone())
+
+                # Single-batch stop
+                if batch_size == 1:
+                    if next_token.item() == self.tokenizer.eos_token:
+                        print_rank0("Single sample reached EOS, stopping.")
+                        break
+                # Multiple-batch stop
+                else:
+                    # Using the view function to flatten the array, in the case next_token is a 2D tensor.
+                    temp_mask = (next_token.view(-1) == self.tokenizer.eos_token)
+                    done_mask |= temp_mask
+                    if done_mask.all():
+                        print_rank0("All samples reached EOS, stopping.")
+                        break
+
         output_tensor = torch.cat(output_tokens, dim=1)
         # End timing and calculate elapsed time
         end.record()
