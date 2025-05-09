d490ee67aaa357b63f3f48b99fd376014aa3db79
keshprad <32313895+keshprad@users.noreply.github.com>
2025-05-09 02:19:21 +0200

    quick fix: flash2 seems to fail when flash3 is imported

--- yalis/attention/backends.py
+++ yalis/attention/backends.py
@@ -1,7 +1,10 @@
 # These imports trigger @register_attention decorators
 from . import sdpa_and_flex
 from . import flash
-from . import fa3
+
+import os
+if os.environ.get("USE_FA3", '0') == '1':
+    from . import fa3
 
 from enum import Enum
 
