ca496612fe56082bf53b9380845a96ea4ededc3d
Prajwal Singhania <prajwal1210@gmail.com>
2025-07-21 09:37:54 -0700

    Test scripts

---
+++ tests/get_rank_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# select_gpu_device wrapper script
+export JOBID=${SLURM_JOB_ID}
+export RANK=${SLURM_PROCID}
+export WORLD_SIZE=${SLURM_NTASKS}
+export LOCAL_RANK=${SLURM_LOCALID}
+export TORCHINDUCTOR_CACHE_DIR="${SCRATCH}/.cache/torch_inductor/rank_${RANK}"
+
+export HF_HOME=$SCRATCH/hf_cache
+export TRANSFORMERS_CACHE="$SCRATCH/hf_cache"
+export HF_DATASETS_CACHE="$SCRATCH/hf_cache"
+
+#cmd="nsys profile -o ./yalistrace_${JOBID}_${RANK} -t cuda,nvtx --capture-range=cudaProfilerApi --capture-range-end=stop --cuda-graph-trace=node $*"
+cmd=$*
+
+exec $cmd
