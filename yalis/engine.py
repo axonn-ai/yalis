182677698d527b22fe253ff50251bf2932fe683f
aKssup <118090347+aKssup@users.noreply.github.com>
2026-02-24 22:41:55 -0500

    remove mark dynamic

--- yalis/engine.py
+++ yalis/engine.py
@@ -389,8 +389,8 @@ class LLMEngine:
                 )
 
                 # Mark B and T dynamic for warmup
-                dynamo.mark_dynamic(tokens, 0, min=1, max=self.inference_config.max_batch_size) # B
-                dynamo.mark_dynamic(tokens, 1, min=1, max=1024) # T
+                # dynamo.mark_dynamic(tokens, 0, min=1, max=self.inference_config.max_batch_size) # B
+                # dynamo.mark_dynamic(tokens, 1, min=1, max=1024) # T
                 # dynamo.mark_dynamic(lens, 0) # B
 
                 # _ = prefill_logits_last(self.model, tokens, lens, EnginePhase.PREFILL)
