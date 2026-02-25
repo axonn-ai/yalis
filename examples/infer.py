667fe66bdbcf76ef7568100d85c6d1d0f7a423b3
aKssup <118090347+aKssup@users.noreply.github.com>
2026-02-24 23:05:56 -0500

    bs1

--- examples/infer.py
+++ examples/infer.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     ]
 
     # take 16 prompts from this dataset
-    user_prompts = user_prompts[:8]
+    user_prompts = user_prompts[:1]
     print(f"Number of prompts = {len(user_prompts)}")
 
     system_prompt = (
